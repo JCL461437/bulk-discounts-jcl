95776b47e4971bdbac1c3c2170b596e1a5ff8ef6
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-07 09:45:18 -0600

    Add bulk_discount.csv data and rake task to load the data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Rake::Task["csv_load:all"].invoke
+
+BulkDiscount.create!(quantity_threshold: 10, percentage_discount: 5.0, merchant: merchant1)
+BulkDiscount.create!(quantity_threshold: 20, percentage_discount: 10.0, merchant: merchant1)
+BulkDiscount.create!(quantity_threshold: 15, percentage_discount: 7.5, merchant: merchant2)
+BulkDiscount.create!(quantity_threshold: 25, percentage_discount: 12.5, merchant: merchant2)
+BulkDiscount.create!(quantity_threshold: 30, percentage_discount: 15.0, merchant: merchant3)
+BulkDiscount.create!(quantity_threshold: 50, percentage_discount: 20.0, merchant: merchant3)
