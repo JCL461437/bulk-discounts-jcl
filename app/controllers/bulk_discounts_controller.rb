405ab1bdbb74da0e896aac8b35dc79c6fcedd964
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-11 14:49:46 -0600

    Add update and edit actions for bulk discounts controller, and added before action for params set bulk discount.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,15 +1,26 @@
 class BulkDiscountsController< ApplicationController
 
   before_action :set_merchant
+  before_action :set_bulk_discount, only: [:show, :edit, :update, :destroy]
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
-    # @discounts = BulkDiscount.where(merchant_id: params[:merchant_id])
   end
 
   def show
     @discount = BulkDiscount.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @bulk_discount.update(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(@bulk_discount), notice: 'Bulk discount was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def new
@@ -39,6 +50,10 @@ class BulkDiscountsController< ApplicationController
 
   def bulk_discount_params
     params.require(:bulk_discount).permit(:percentage_discount, :quantity_threshold)
+  end
+
+  def set_bulk_discount
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
 end
