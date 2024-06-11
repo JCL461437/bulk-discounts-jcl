7cdd8b75d12825bf8c91a894c9d331e46ba0d9e4
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-10 18:26:42 -0600

    Add correct view layout with form_with and proper controller actions.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,4 +1,7 @@
 class BulkDiscountsController< ApplicationController
+
+  before_action :set_merchant
+
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
@@ -8,4 +11,28 @@ class BulkDiscountsController< ApplicationController
   def show
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @bulk_discount = @merchant.bulk_discounts.new
+  end
+
+  def create
+    @bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    if @bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant), notice: 'Bulk discount was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percentage_discount, :quantity_threshold)
+  end
+
 end
